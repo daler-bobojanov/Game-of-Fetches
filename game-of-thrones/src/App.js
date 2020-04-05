95430d824ffcaab6e02b78b384e5cd648f2835bb
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-05 19:23:47 -0400

    added code for the last component

--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -6,6 +6,7 @@ import HouseLCoatOfArms from './components/HouseLCoatOfArms';
 import SecSeatOfHouseBar from './components/SecSeatOfHouseBar';
 import RobertBSecondAlias from './components/RobertBSecondAlias';
 import FounderOfHouseStark from './components/FounderOfHouseStark';
+import TitlesOfCatelynSPOVBooks from './components/TitlesOfCatelynS3POVBooks';
 
 class App extends Component {
   render() {
@@ -18,6 +19,7 @@ class App extends Component {
         <SecSeatOfHouseBar />
         <RobertBSecondAlias />
         <FounderOfHouseStark />
+        <TitlesOfCatelynSPOVBooks />
       </div>
     );
   }
@@ -27,25 +29,25 @@ export default App;
 
 /*
 Here's the part you have to do
-
+1.
 Where was Margaery Tyrell born?
 URL: http://anapioficeandfire.com/api/characters/16
-
+2.
 What region is House Targaryen in?
 URL: http://www.anapioficeandfire.com/api/houses/378
-
+3.
 What's the coat of arms of House Lannister?
 URL: http://www.anapioficeandfire.com/api/houses/229
-
+4.
 What is the second seat of House Baratheon?
 URL: http://www.anapioficeandfire.com/api/houses/17
-
+5.
 What is Robert Baratheon's second alias?
 URL: http://www.anapioficeandfire.com/api/characters/901
-=====
+6.
 What's the name of the founder of House Stark? (You have to chain fetch requests!)
 URL: http://www.anapioficeandfire.com/api/houses/362
-
+7.
 What are the titles of Catelyn Stark's three POV books? (Look into Promise.all to request these simultaniously)
 URL: http://www.anapioficeandfire.com/api/characters/232
 */
